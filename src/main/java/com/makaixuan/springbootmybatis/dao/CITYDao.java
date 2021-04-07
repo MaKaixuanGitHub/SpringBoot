73042f34f3eaddea599966ecf87c73572977b44c
MaKaixuanGitHub <makaixuan25@gmial.com>
2021-04-07 10:47:19 +0800

    makaixuan update cityDao

--- src/main/java/com/makaixuan/springbootmybatis/dao/CITYDao.java
+++ src/main/java/com/makaixuan/springbootmybatis/dao/CITYDao.java
@@ -1,16 +1,17 @@
 package com.makaixuan.springbootmybatis.dao;
 
 import com.makaixuan.springbootmybatis.entity.CITY;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface CITYDao {
 
-    public CITY findById(int id);
+    public CITY findById(@Param("id") int id);
 
     public List<CITY> getCities();
 
-    public int deleteById(int id);
+    public int deleteById(@Param("id") int id);
 
     public int updateById(CITY city);
 
